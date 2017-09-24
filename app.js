e918c8e56dec167829bff0612dad017d0a7c16a0
joaquinjimenezgarcia <garcia.jjimenez@gmail.com>
2017-09-24 20:34:16 +0200

    Anyadido generador de vistas Handlebars y configurado

--- app.js
+++ app.js
@@ -3,10 +3,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const api = require('./routes/document')
+const hbs = require('express-handlebars')
 const app = express()
 
 app.use(bodyParser.urlencoded( { extended: false } ))
 app.use(bodyParser.json())
+
+app.engine('.hbs', hbs({
+  defaultLayout: 'default',
+  extname: '.hbs'
+}))
+app.set('view engine', '.hbs')
+
 app.use('/api', api)
 
 module.exports = app
