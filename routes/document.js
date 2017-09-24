2c5f0c91b21492cf0fd044a8f1f2920a1220da5a
joaquinjimenezgarcia <garcia.jjimenez@gmail.com>
2017-09-24 18:28:00 +0200

    - Anadido middleware para autenticacion

--- routes/document.js
+++ routes/document.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const documentController = require('../controllers/document')
+const auth = require('../middlewares/auth')
 const api = express.Router()
 
 api.get('/document', documentController.getDocuments)
@@ -9,5 +10,9 @@ api.get('/document/:documentId', documentController.getDocument)
 api.post('/document', documentController.saveDocument)
 api.put('/document/:documentId', documentController.updateDocument)
 api.delete('/document/:documentId', documentController.deleteDocument)
+
+api.get('/private', auth.isAuth, (req, res) => {
+  res.status(200).send({message:'Tienes acceso'})
+})
 
 module.exports = api
